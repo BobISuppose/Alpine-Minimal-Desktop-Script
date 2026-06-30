e742ecc8c6034b442c2063ac59cf27281e5adcad
BobISuppose <82328576+BobISuppose@users.noreply.github.com>
2026-06-29 22:09:08 -0700

    Add desktop environment setup script
    Install X11 and Wayland desktop environments along with basic fonts. Configure IceWM session for X11.

---
+++ step2-desktop.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -e
+
+echo "=== Step 2: Desktop Environment ==="
+
+# X11 stack for IceWM + Tiles2
+apk add \
+    xorg-server \
+    xinit \
+    icewm \
+    tiles2 \
+    pcmanfm \
+    lxterminal
+
+# Wayland stack (Wayfire)
+apk add \
+    wayfire \
+    waybar \
+    wlogout \
+    wlr-randr \
+    wf-shell \
+    foot \
+    grim slurp \
+    swaybg \
+    swaylock \
+    swayidle
+
+# Basic fonts
+apk add \
+    font-dejavu \
+    font-noto \
+    font-noto-emoji
+
+# Enable IceWM session
+cat <<EOF >/etc/X11/xinit/xinitrc
+#!/bin/sh
+exec icewm-session
+EOF
+
+chmod +x /etc/X11/xinit/xinitrc
