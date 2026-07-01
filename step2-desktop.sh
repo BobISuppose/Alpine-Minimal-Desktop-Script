816f02255895cc60f69208619e553a22f8035dd0
BobISuppose <82328576+BobISuppose@users.noreply.github.com>
2026-06-30 22:11:02 -0700

    Remove IceWM stack from desktop setup script
    Removed IceWM and related packages from the installation script.

--- step2-desktop.sh
+++ step2-desktop.sh
@@ -3,15 +3,6 @@
 
 echo "=== Step 2: Desktop Environment ==="
 
-# X11 stack for IceWM + Tiles2
-apk add \
-    xorg-server \
-    xinit \
-    icewm \
-    tiles2 \
-    pcmanfm \
-    lxterminal
-
 # Wayland stack (Wayfire)
 apk add \
     wayfire \
@@ -31,10 +22,3 @@ apk add \
     font-noto \
     font-noto-emoji
 
-# Enable IceWM session
-cat <<EOF >/etc/X11/xinit/xinitrc
-#!/bin/sh
-exec icewm-session
-EOF
-
-chmod +x /etc/X11/xinit/xinitrc
